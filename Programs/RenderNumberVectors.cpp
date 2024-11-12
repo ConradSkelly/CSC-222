82b88a3a7a182c6e3ed607343386b7be6ae76b52
ConradSkelly <conradskelly@gmail.com>
2024-11-12 14:50:26 -0500

    made p1 &2 work

--- Programs/RenderNumberVectors.cpp
+++ Programs/RenderNumberVectors.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 string render_num_vector(const vector<int>& numbers) {
-    return "";
+    string output = "";
+    if (numbers.size() == 0)
+      return output;
+
+    for (int element : numbers)
+        output += to_string(element) + " ";
+
+    output.pop_back();
+
+  return output;
 }
--- Programs/TestRenderVector.cpp
+++ Programs/TestRenderVector.cpp
@@ -8,7 +8,21 @@ using namespace std;
 TEST_CASE("Testing render_num_vector") {
     vector<int> nums = {1, 3, 7};
     string numstr = render_num_vector(nums);
-    string expected = "1 3 7";
+    string expected1 = "1 3 7";
+    CHECK(numstr == expected1);
 
-    CHECK(numstr == expected);
+    vector<int> nums2 = {1, 2, 3, 4};
+    string numstr2 = render_num_vector(nums2);
+    string expected2 = "1 2 3 4";
+    CHECK(numstr2 == expected2);
+
+    vector<int> nums3 = {};
+    string numstr3 = render_num_vector(nums3);
+    string expected3 = "";
+    CHECK(numstr3 == expected3);
+}
+
+TEST_CASE("Testing num_vector_sum") {
+    vector<int> nums = {1, 3, 7};
+    CHECK(11 == num_vector_sum(nums));
 }
