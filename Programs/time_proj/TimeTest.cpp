9bfae63b099ef53aaad8b534bd4fd539f6550100
ConradSkelly <conradskelly@gmail.com>
2024-11-18 13:25:15 -0500

    made the test work

--- Programs/time_proj/TimeTest.cpp
+++ Programs/time_proj/TimeTest.cpp
@@ -6,7 +6,7 @@
 #include "time.h"
 
 TEST_CASE("Testing render_num_vector") {
-    // Time nums = {1, 3, 7};
-    // string orderTime = printTime(nums);
-    CHECK(false);
+    Time nums = {1, 3, 7};
+    string orderTime = printTime(nums);
+    CHECK("1:3:7");
 }
--- Programs/time_proj/time.cpp
+++ Programs/time_proj/time.cpp
@@ -13,6 +13,6 @@ using namespace std;
 string printTime(const Time& time) {
   string outputTime = to_string(time.hour) + ":" + to_string(time.minute) + ":" + to_string(time.second);
   return outputTime;
-    cout << time.hour << ":" << time.minute  << ":" << time.second << endl;
+  cout << time.hour << ":" << time.minute  << ":" << time.second << endl;
 
 }
