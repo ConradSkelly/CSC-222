8f4db0bf932c9c92811826ef5143ac9595980376
ConradSkelly <conradskelly@gmail.com>
2024-12-03 14:24:01 -0500

    added test and made them work

--- Programs/TempleteClassProj/DoctestTempleteClass.cpp
+++ Programs/TempleteClassProj/DoctestTempleteClass.cpp
@@ -15,4 +15,14 @@ TEST_CASE("testing swap function") {
     auto swap = t3.getPair();
     CHECK(swap.first == 5);
     CHECK(swap.second == 1);
+
+    TempleteClass t4(1.5,5.3);
+    auto swap2 = t4.getPair();
+    CHECK(swap2.first == 5.3);
+    CHECK(swap2.second == 1.5);
+
+    TempleteClass t5("world","Hello");
+    auto swap3 = t5.getPair();
+    CHECK(swap3.first == "Hello");
+    CHECK(swap3.second == "world");
 }
